a74408c79385eb20a8cbfe8073ae477b138a7ecc
Bida23-083 <bac@thuto.bac.ac.bw>
2026-05-05 16:03:14 +0200

    fix CORS for github pages

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://bida23-083.github.io"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
